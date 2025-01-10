3643b21141bf7655fe02deccc55e49db4bd1fdcf
ragOP <najmiraghib@gmail.com>
2025-01-10 19:42:13 +0530

    name changes

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 const Chat = () => {
   useEffect(() => {
     // Redirect to second.html
-    window.location.href = '/second.html';
+    window.location.href = '/engchat1.html';
   }, []);
 
   return null; // This component won't render anything
--- src/pages/Chat2.tsx
+++ src/pages/Chat2.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 const Chat = () => {
   useEffect(() => {
     // Redirect to second.html
-    window.location.href = '/third.html';
+    window.location.href = '/engchat25k.html';
   }, []);
 
   return null; // This component won't render anything
